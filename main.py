f47e7d83fb6331074200967eaf8dc6cddfd34989
William Pierce <wpierce118@gmail.com>
2023-02-10 21:49:59 -0800

    Error handling, reorganized parser

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import pandas as pd
 
 class FitDiff:
     def __init__(self, user: str, doctrine: str) -> None:
-        self.user = self._parse_multibuy_file(user)
-        self.doctrine = self._parse_contents_file(doctrine)
+        self.user = self._parse_input_file(
+            filepath=user,
+            type="multibuy",
+        )
+        self.doctrine = self._parse_input_file(
+            filepath=doctrine,
+            type="contents",
+        )
         self.quantity_diff = self._create_quantity_diff()
         self.summary_tables = self._create_summary_tables()
 
@@ -16,54 +22,37 @@ class FitDiff:
         ]
         return "".join(lines)
 
-    @staticmethod
-    def _parse_contents_file(
+    def _parse_input_file(
+        self,
         filepath: str,
+        type: str,
         item_col: str = "item",
         quantity_col: str = "quantity",
     ) -> pd.DataFrame:
         try:
-            df = pd.read_csv(
-                filepath,
-                header=None,
-                names=[item_col, "type", "location", quantity_col],
-                usecols=[item_col, quantity_col],
-                delimiter="\t",
-            )
-
-            df[item_col] = df[item_col].str.strip()
-            df[quantity_col] = df[quantity_col].astype(int)
-            df = (
-                df.groupby(item_col, as_index=False)
-                .agg({"quantity": "sum"})
-                .sort_values("item")
-            )
-            return df
-        except (
-            FileNotFoundError,
-            pd.errors.EmptyDataError,
-            pd.errors.ParserError,
-        ) as e:
-            print(f"Error: {e}")
-            return pd.DataFrame()
-
-    @staticmethod
-    def _parse_multibuy_file(filepath: str) -> pd.DataFrame:
-        try:
-            df = (
-                pd.read_csv(
+            if type == "contents":
+                df = pd.read_table(
+                    filepath,
+                    header=None,
+                    names=[item_col, "type", "location", quantity_col],
+                    usecols=[item_col, quantity_col],
+                    dtype={quantity_col: "int"},
+                )
+            elif type == "multibuy":
+                df = pd.read_table(
                     filepath,
                     header=None,
                     sep=r"[\s][x](?=\d)",
-                    names=["item", "quantity"],
+                    names=[item_col, quantity_col],
                     engine="python",
-                )
-                .fillna(1)
-                .groupby("item", as_index=False)
-                .agg({"quantity": "sum"})
-                .sort_values("item")
+                ).fillna(1)
+            else:
+                pass
+            return (
+                df.groupby(item_col, as_index=False)
+                .agg({quantity_col: "sum"})
+                .sort_values(item_col)
             )
-            return df
         except (
             FileNotFoundError,
             pd.errors.EmptyDataError,
@@ -73,53 +62,47 @@ class FitDiff:
             return pd.DataFrame()
 
     def _create_quantity_diff(self) -> pd.DataFrame:
-        df = pd.merge(
-            self.user,
-            self.doctrine,
-            on="item",
-            how="outer",
-            suffixes=["_user", "_doctrine"],
-        ).fillna(0)
-        df[["quantity_user", "quantity_doctrine"]] = df[
-            ["quantity_user", "quantity_doctrine"]
-        ].astype(int)
-        df = df.assign(
-            quantity_diff=df["quantity_user"] - df["quantity_doctrine"]
-        ).sort_values("item")
-        return df
+        try:
+            df = pd.merge(
+                self.user,
+                self.doctrine,
+                on="item",
+                how="outer",
+                suffixes=["_user", "_doctrine"],
+            ).fillna(0)
+            df[["quantity_user", "quantity_doctrine"]] = df[
+                ["quantity_user", "quantity_doctrine"]
+            ].astype(int)
+            df = df.assign(
+                quantity_diff=df["quantity_user"] - df["quantity_doctrine"]
+            ).sort_values("item")
+            return df
+        except (KeyError) as e:
+            print(f"Error: {e}")
+            return pd.DataFrame()
 
     def _create_summary_tables(self) -> dict:
-        correct_items = self._create_correct_items_table()
-        missing_items = self._create_missing_items_table()
-        extra_items = self._create_extra_items_table()
-        return {
-            "Correct Items": correct_items,
-            "Missing Items": missing_items,
-            "Extra Items": extra_items,
-        }
-
-    def _create_correct_items_table(self) -> pd.DataFrame:
-        correct_items = self.quantity_diff.query("quantity_diff == 0")[
-            ["item", "quantity_doctrine"]
-        ].reset_index(drop=True)
-        correct_items = correct_items.rename(columns={"quantity_doctrine": "quantity"})
-        return correct_items
-
-    def _create_missing_items_table(self) -> pd.DataFrame:
-        missing_items = self._create_summary(self.quantity_diff["quantity_diff"] < 0)
-        return missing_items
-
-    def _create_extra_items_table(self) -> pd.DataFrame:
-        extra_items = self._create_summary(self.quantity_diff["quantity_diff"] > 0)
-        return extra_items
-
-    def _create_summary(self, mask: pd.Series) -> pd.DataFrame:
-        summary = self.quantity_diff[mask][["item", "quantity_diff"]].reset_index(
-            drop=True
-        )
-        summary = summary.rename(columns={"quantity_diff": "quantity"})
-        summary["quantity"] = summary["quantity"].abs()
-        return summary
+        try:
+            correct_items = self.quantity_diff.query("quantity_diff == 0").reset_index(
+                drop=True
+            )
+            missing_items = self.quantity_diff[
+                self.quantity_diff["quantity_diff"] < 0
+            ].reset_index(drop=True)
+            extra_items = self.quantity_diff[
+                self.quantity_diff["quantity_diff"] > 0
+            ].reset_index(drop=True)
+            return {
+                "Correct Items": correct_items,
+                "Missing Items": missing_items,
+                "Extra Items": extra_items,
+            }
+        except (
+            KeyError,
+            pd.errors.UndefinedVariableError,
+        ) as e:
+            print(f"Error: {e}")
+            return pd.DataFrame()
 
 
 diff = FitDiff(
