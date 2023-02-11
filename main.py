1e6c3fec023aa8d01ca725439992558630226ca9
William Pierce <wpierce118@gmail.com>
2023-02-10 20:11:38 -0800

    Renaming

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 class FitDiff:
     def __init__(self, user: str, doctrine: str) -> None:
-        self.user = self._parse_multibuy(user)
-        self.doctrine = self._parse_contents(doctrine)
-        self.diff = self._diff_quantity()
+        self.user = self._parse_multibuy_file(user)
+        self.doctrine = self._parse_contents_file(doctrine)
+        self.diff = self._create_quantity_diff()
         self.summary_tables = self._create_summary_tables()
 
     def __str__(self) -> str:
@@ -17,7 +17,7 @@ class FitDiff:
         return returnstr
 
     @staticmethod
-    def _parse_contents(filepath: str) -> pd.DataFrame:
+    def _parse_contents_file(filepath: str) -> pd.DataFrame:
         contents = pd.read_csv(
             filepath,
             header=None,
@@ -33,7 +33,7 @@ class FitDiff:
         return contents.sort_values("item")
 
     @staticmethod
-    def _parse_multibuy(filepath: str) -> pd.DataFrame:
+    def _parse_multibuy_file(filepath: str) -> pd.DataFrame:
         return (
             pd.read_csv(filepath, header=None)[0]
             .str.split(r"([\s])[x](?=\d)", expand=True)
@@ -46,14 +46,14 @@ class FitDiff:
             .sort_values("item")
         )
 
-    def _diff_quantity(self) -> pd.DataFrame:
+    def _create_quantity_diff(self) -> pd.DataFrame:
         joined = pd.merge(
             self.user,
             self.doctrine,
             on="item",
             how="outer",
             suffixes=["_user", "_doctrine"],
-        ).fillna(value=0)
+        ).fillna(0)
         joined[["quantity_user", "quantity_doctrine"]] = joined[
             ["quantity_user", "quantity_doctrine"]
         ].astype(int)
@@ -61,12 +61,12 @@ class FitDiff:
         return diff.sort_values("item")
 
     def _create_summary_tables(self) -> dict:
-        correct = self.diff.query("diff == 0")[
+        correct_items = self.diff.query("diff == 0")[
             ["item", "quantity_doctrine"]
         ].reset_index(drop=True)
-        correct = correct.rename(columns={"quantity_doctrine": "quantity"})
+        correct_items = correct_items.rename(columns={"quantity_doctrine": "quantity"})
 
-        def create_summary(df, operator):
+        def create_summary(df: pd.DataFrame, operator: str) -> pd.DataFrame:
             summary = df.query(f"diff {operator} 0")[["item", "diff"]].reset_index(
                 drop=True
             )
@@ -74,10 +74,14 @@ class FitDiff:
             summary["quantity"] = summary["quantity"].abs()
             return summary
 
-        needed = create_summary(self.diff, "<")
-        extra = create_summary(self.diff, ">")
+        missing_items = create_summary(self.diff, "<")
+        extra_items = create_summary(self.diff, ">")
 
-        return {"correct": correct, "needed": needed, "extra": extra}
+        return {
+            "Correct Items": correct_items,
+            "Missing Items": missing_items,
+            "Extra Items": extra_items,
+        }
 
 
 diff = FitDiff(
